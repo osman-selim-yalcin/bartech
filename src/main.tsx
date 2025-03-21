1299bd4604f93ba0a2f2a2aa6d3bf06bba27bef6
osman yalçın <osmanyalcin989@gmail.com>
2025-03-21 14:28:27 +0300

    lala

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./main.css";
+import "./Main.css";
 import { MainRoutes } from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
